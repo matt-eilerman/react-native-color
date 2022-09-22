4cd28661d60f83a580b83efde4a2783806117fb0
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-21 22:44:09 -0700

    Remove Valueables Not in Use For Gradient Slider

--- src/sliders/GradientSlider.js
+++ src/sliders/GradientSlider.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Slider from 'react-native-slider';
-import Gradient from '../gradients/Gradient';
 
 const GradientSlider = ({
   style,
